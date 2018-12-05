1505205c9c90ee78e7d80f73f342063c6665ec9a
Giulia Santucci <z5178033@Giulias-MacBookPro.local>
2018-12-06 10:05:18 +1100

    modified plot function

---
+++ customroutines/test_plot.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+from plot import plot
+x= [2,-3, 4, 5.3, 9.6, -12, -8.4, 2.4, 10, 3.4]
+y= [2,-3, 4, 5.3, 9.6, -12, -8.4, 2.4, 10, 3.4]
+plot (x, y, 'random numbers', 'function', 'test', 3.4, 0.032 )
