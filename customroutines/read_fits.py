59b8cc5dd26a7686c34bab75c05c585f01d6d044
Anshu Gupta <mail02anshu@gmail.com>
2018-12-06 10:45:01 +1100

    anshu_20181206_1044

--- customroutines/read_fits.py
+++ customroutines/read_fits.py
@@ -29,9 +29,39 @@ class Read_fits(object):
 		if getattr(self,'_ivar',None) is None:
 			self._error = self.hdu[1].data['ivar']
 		return self._error
+		
+
+class Galaxy_info(object):
+	def __init__(self, filename):
+		if filename is None:
+			print("No filename specified")
+		else:
+			self.filename = filename
+			self.hdu = fits.open(self.filename)
+		
+	@property
+	def redshift(self):
+		if getattr(self,'_LINEZ',None) is None:
+			self._redshift = dict()
+			self._redshift["z"] = self.hdu[3].data['LINEZ']
+			self._redshift["z_err"] = self.hdu[3].data['LINEZ_ERR']
+			#print(self.hdu[3].data['LINEZ'])
+			return self._redshift
+		
+	@property
+	def spectral_lines(self):
+		if getattr(self,'_LINENAME',None) is None:
+			self._spectral_lines = dict()
+			self._spectral_lines["linename"] = self.hdu[3].data['LINENAME']
+			self._spectral_lines["ew"] = self.hdu[3].data['LINEEW']
+			self._spectral_lines["ew_err"] = self.hdu[3].data['LINEEW_ERR']
+			#print(self.hdu[3].data['LINEZ'])
+			return self._spectral_lines
+		
 
 
-filename = '/Users/z3525264/Documents/work/SciCoder-2018-Sydney/sdss_scicoder/spectra/spec-10000-57346-0002.fits'
 
-sdss_galaxy = Read_fits(filename)
-print(sdss_galaxy.wavelength, sdss_galaxy.flux, sdss_galaxy.error)
+#filename = '/Users/z3525264/Documents/work/SciCoder-2018-Sydney/sdss_scicoder/spectra/spec-10000-57346-0002.fits'
+
+#sdss_galaxy = Galaxy_info(filename)
+#print(sdss_galaxy.spectral_lines["linename"])
