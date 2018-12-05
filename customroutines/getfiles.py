d4b51f02d8af50281d722fd053d9da640e81a873
anishya harshan <anishya13@gmail.com>
2018-12-06 09:55:35 +1100

    added import statements -- this will be the b0ody of our code

--- customroutines/getfiles.py
+++ customroutines/getfiles.py
@@ -1,4 +1,6 @@
 import argparse
+import plot.py as pl
+import read_fits.py as rf
 parser = argparse.ArgumentParser(description = "This program reads sdss spectrum and plots them", usage = "final_module_name --filename <filename.fits> ")
 parser.add_argument("-f","--filename", help = "Path to the spectrum fits file")
 args = parser.parse_args()
